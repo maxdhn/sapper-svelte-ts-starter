cce53fd62a3e29e00839f7cecea02501a81077f1
maxdhn <maxduchan@gmail.com>
2020-02-02 15:21:23 +0100

    prettier

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,19 +1,19 @@
-<script lang="typescript">
-  export let value: number = 0;
-  export let step: number = 1;
-</script>
-
 <style type="text/css">
-  .my-tailwind-class {
-    @apply bg-transparent bg-blue-500 text-blue-700 font-semibold text-white py-2 px-4 border border-blue-500 border-transparent rounded
-  }
+    .my-tailwind-class {
+        @apply bg-transparent bg-blue-500 text-blue-700 font-semibold text-white py-2 px-4 border border-blue-500 border-transparent rounded;
+    }
 </style>
 
+<script lang="typescript">
+    export let value: number = 0;
+    export let step: number = 1;
+</script>
+
 <div>
-  <slot>Default Counter</slot>
-  <div>
-    {value}
-    <button class="my-tailwind-class" on:click={() => (value += step)}>+</button>
-    <button on:click={() => (value -= step)}>-</button>
-  </div>
+    <slot>Default Counter</slot>
+    <div>
+        {value}
+        <button class="my-tailwind-class" on:click="{() => (value += step)}">+</button>
+        <button on:click="{() => (value -= step)}">-</button>
+    </div>
 </div>
